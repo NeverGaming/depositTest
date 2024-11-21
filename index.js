44993523fbff6c5bedfbb309ff6f18515ba74ab5
seddumf <seddumf@qq.com>
2024-11-21 20:29:00 +0800

    简单的netlify测试

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { formatDistanceToNow } from "date-fns";
 
 const date = "2004-12-31 10:00:00";
-document.body.textContent = `${formatDistanceToNow(new Date(date))} ago`;
+document.body.textContent = `${formatDistanceToNow(new Date(date))} ago, <另外，这是部署Netlify时所做的修改>`;
